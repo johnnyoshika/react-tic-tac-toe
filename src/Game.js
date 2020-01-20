c8e41e5d6ea63f7de180fa02b3196bd470d832f5
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-20 10:45:29 -0800

    Implement time travel

--- src/Game.js
+++ src/Game.js
@@ -34,22 +34,35 @@ const Game = () => {
     },
   ]);
 
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : `Go to game start`;
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   const status = winner
     ? `Winner: ${winner}`
     : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   const handleClick = i => {
-    const current = history[history.length - 1];
+    const clone = history.slice(0, stepNumber + 1);
+    const current = clone[clone.length - 1];
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[i]) return;
 
     squares[i] = xIsNext ? 'X' : 'O';
-    setHistory(history.concat([{ squares: squares }]));
+    setHistory(clone.concat([{ squares: squares }]));
+    setStepNumber(clone.length);
     setXIsNext(!xIsNext);
   };
 
@@ -59,7 +72,13 @@ const Game = () => {
         squares: start,
       },
     ]);
+    setStepNumber(0);
     setXIsNext(true);
+  };
+
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
   };
 
   return (
@@ -72,7 +91,7 @@ const Game = () => {
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
