7fd25aa9fd3396a7e19377fe94e7ef17eb160f73
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-20 09:00:34 -0800

    Display 'X' when button is clicked

--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Square = () => {
-  return <button className="square">{/* TODO */}</button>;
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" onClick={() => setValue('X')}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
